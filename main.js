e60850c47401d72534b7c4e7090457986a60c185
rkdunlop <rdunlop@HS14-Shop-T.ptjeffad.portjeff.k12.ny.us>
2015-05-14 15:00:37 -0400

    updated buttons

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var connectionId = -1;
-
+var str='';
 window.onload = function() {
   document.querySelector('#greeting').innerText =
-    'Hello, World! It is ' + new Date();
+    'Port Jefferson MagLev Timer';
     chrome.serial.getDevices(function(ports){
     onGetDevices(ports);
     buildPortPicker(ports);
     buildResetButton();
+    buildConnectButton();
     document.getElementById('secs').innerText = '0000';
     });
 
@@ -19,12 +20,15 @@ var onGetDevices = function(ports) {
 };
 var stringReceived = '';
 function onRead(readInfo){
-  
-    var str = convertArrayBufferToString(info.data);
+    //var uint8View = new Uint8Array(readInfo.data);
+    var str = String.fromCharCode.apply(null, new Uint8Array(readInfo.data));
+    //var str = convertArrayBufferToString(readInfo.data);
     if (str.charAt(str.length-1) === '\n') {
       stringReceived += str.substring(0, str.length-1);
-      displaySecs(parseFloat(stringReceived));
-      stringReceived = '';
+      console.log(parseFloat(stringReceived));
+      //displaySecs(stringReceived);
+      //displaySecs(parseFloat(stringReceived));
+      stringReceived = ' ';
     }else {
       stringReceived += str;
     }
@@ -33,7 +37,9 @@ function onRead(readInfo){
 
 
 function displaySecs(number) {
+  number = parseFloat(number);
   number = number/1000;
+  
   document.getElementById('secs').innerText = number;
 }
 var onError = function(errorInfo)  {
@@ -43,6 +49,7 @@ var onError = function(errorInfo)  {
 function onOpen(openInfo) {
   connectionId = openInfo.connectionId;
   console.log("connectionId: " + connectionId);
+  console.log("bitrate: " +openInfo.bitrate);
   if (connectionId == -1) {
     setStatus('Could not open');
     return;
@@ -50,6 +57,7 @@ function onOpen(openInfo) {
   setStatus('Connected');
 
   chrome.serial.onReceive.addListener(onRead);
+  
 }
 
 function setStatus(status) {
@@ -90,14 +98,38 @@ function buildResetButton() {
   var resetButton = document.getElementById('reset');
   
   resetButton.onclick = function() {
-    console.log(reset);
-    displaySecs(0);
-    chrome.serial.disconnect(connectionId,openSelectedPort);
+    console.log('reset');
+    document.getElementById('secs').innerText = '0000';
+    console.log(connectionId);
+    stringReceived = ' ';
+    str ='';
+    chrome.serial.flush(connectionId,onFlush);
+    chrome.serial.disconnect(connectionId,onDisconnect);
+    openSelectedPort();
   };
+var onDisconnect = function(result) {
+  if (result) {
+    console.log("Disconnected from the serial port");
+  } else {
+    console.log("Disconnect failed");
+  }
+};
   
   
 }
-
+function onFlush(flush) {
+  return flush;
+}
+function buildConnectButton() {
+  var connectButton = document.getElementById('connect');
+  
+  connectButton.onclick = function() {
+    console.log('connect');
+    var portPicker = document.getElementById('port-picker');
+  var selectedPort = portPicker.options[portPicker.selectedIndex].value;
+  chrome.serial.connect(selectedPort,{bitrate:9600}, onOpen);
+  };
+}
 
 
 function openSelectedPort() {
